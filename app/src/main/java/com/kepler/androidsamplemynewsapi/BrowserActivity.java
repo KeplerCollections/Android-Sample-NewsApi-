f2d0bffc698a738a8a25ab03119a418570c76fdf
carlsberg <amitjaiswal002@gmail.com>
2020-04-16 23:32:21 +0530

    Minor UI Changes on Browser

--- app/src/main/java/com/kepler/androidsamplemynewsapi/BrowserActivity.java
+++ app/src/main/java/com/kepler/androidsamplemynewsapi/BrowserActivity.java
@@ -1,9 +1,12 @@
 package com.kepler.androidsamplemynewsapi;
 
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.kepler.androidsamplemynewsapi.boilers.BaseActivity;
 
@@ -15,6 +18,8 @@ public class BrowserActivity extends BaseActivity {
 
     @BindView(R.id.webview)
     WebView webView;
+   @BindView(R.id.progrssBar)
+   ProgressBar progrssBar;
     private String url;
 
     @Override
@@ -30,7 +35,33 @@ public class BrowserActivity extends BaseActivity {
 
         // WebViewClient allows you to handle
         // onPageFinished and override Url loading.
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressing(true);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressing(false);
+            }
+        });
+    }
+
+    private void progressing(boolean progressing)
+    {
+        if(progrssBar==null)
+            return;
+        if(progressing){
+            progrssBar.setIndeterminate(true);
+            progrssBar.setVisibility(View.VISIBLE);
+        }else {
+            progrssBar.setVisibility(View.GONE);
+            progrssBar.setIndeterminate(false);
+        }
     }
 
     @Override
@@ -38,4 +69,9 @@ public class BrowserActivity extends BaseActivity {
         return R.layout.activity_web_view;
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        webView.destroy();
+    }
 }
